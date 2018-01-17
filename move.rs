f3f9377faee387a708eb52d5e66d943243925bf2
Andrei Cristian Petcu <andrei@ceata.org>
2018-01-17 21:53:26 +0200

    created path

--- move.rs
+++ move.rs
@@ -5,23 +5,16 @@
 // This code is editable, feel free to hack it!
 // You can always return to the original code by clicking the "Reset" button ->
 use std::env;
+use std::path::Path;
 // This is the main function
 fn main() {
-    // The statements here will be executed when the compiled binary is called
-
-    // Print text to the console
-    println!("Hello World2!");
-
-    match env::var("TR_TORRENT_DIR") {
-        Ok(dir) => println!("TR_TORRENT_DIR: {}", dir),
-        Err(e) => println!("Couldn't read TR_TORRENT_DIR ({})", e),
-    };
-
-    // match env::var("TR_TORRENT_NAME") {
-    //     Ok(name) => println!("TR_TORRENT_NAME: {}", name),
-    //     Err(e) => println!("Couldn't read TR_TORRENT_NAME ({})", e),
-    // };
-
     let name = env::var("TR_TORRENT_NAME").unwrap();
+    let dir = env::var("TR_TORRENT_DIR").unwrap();
     println!("TR_TORRENT_NAME: {}", name);
+    println!("TR_TORRENT_DIR: {}", dir);
+
+    let result = [dir, name].join("/");
+    println!("result: {}", result);
+    
+    let _path = Path::new(&result);
 }
